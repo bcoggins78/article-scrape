9fe928bc439d71264b984baa8f2f2fd87df1b407
bcoggins78 <bcoggins78@gmail.com>
2019-06-14 23:27:34 -0400

    Configured Scraper to use gamespot.com

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,47 @@
-var db = require("../models");
+const db = require("../models");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-module.exports = function(app) {
+
+module.exports = function (app) {
+
+    app.get("/scrape", function (req, res) {
+
+        axios.get("https://www.gamespot.com/news/").then(function (response) {
+            var $ = cheerio.load(response.data);
 
 
+
+            $("article.media-article").each(function (i, element) {
+
+                var result = {};
+
+                result.title = $(this).find("h3.media-title").text();
+                result.link = $(this).children("a").attr("href");
+                result.desc = $(this).find(".media-deck").text();
+                result.img = $(this).find("img").attr("src");
+
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        console.log(dbArticle);
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            });
+
+        });
+
+
+
+
+
+
+
+
+
+
+    })
 
 
 };
