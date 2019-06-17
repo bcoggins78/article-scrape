8fd9196276d9006f485435f2af935bef6cf6dc09
bcoggins78 <bcoggins78@gmail.com>
2019-06-17 00:26:26 -0400

    Removed the Navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <v-app>
-    <Navbar />
     <router-view/>
   </v-app>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
-  components: {
-    Navbar
-  },
   data () {
     return {
       
