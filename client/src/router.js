aa761a05c8b870925889500d6a2d55a5f4f70d11
bcoggins78 <bcoggins78@gmail.com>
2019-06-17 00:27:33 -0400

    Added route for saved view

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Saved from './views/Saved.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/saved',
+      name: 'saved',
+      component: Saved
     }
   ]
 })
